d448976eba2e7b0264a8f1d265080d26fa5587ec
hYuan <wanghaiyuan@jd.com>
2019-01-27 18:55:17 +0800

    maven profile

---
+++ src/test/java/Test.java
@@ -0,0 +1,9 @@
+/**
+ * Author wanghaiyuan
+ * Date 2019/1/27 15:50.
+ */
+public class Test {
+    public static  void main(String[] args){
+        System.out.println("this is test...");
+    }
+}
